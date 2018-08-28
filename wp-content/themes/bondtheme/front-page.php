3fa39039ee3c144e0f5333d7b7cd7866da77f731
Vyacheslav <vasilenko.vyacheslav@catenamedia.com>
2018-08-28 18:25:14 +0300

    popup done

--- wp-content/themes/bondtheme/front-page.php
+++ wp-content/themes/bondtheme/front-page.php
@@ -1,5 +1,44 @@
 <?php get_header(); ?>
-
-Test home page
+<div class="wrapper">
+    <div class="main-nav">
+        <div class="logo">
+            <img src="/wp-content/themes/bondtheme/images/logo.svg" alt="Bond next door">
+        </div>
+        <nav class="navigation">
+            <ul>
+                <li class=""><a href="#">Handsome</a></li>
+                <li class=""><a href="#">Charming</a></li>
+                <li class=""><a href="#">Sense of humour</a></li>
+                <li class=""><a href="#">Confidence</a></li>
+                <li class=""><a href="#">Gentlemanly</a></li>
+                <li class=""><a href="#">Risk taker</a></li>
+                <li class=""><a href="#">Good in a crisis</a></li>
+                <li class=""><a href="#">Flirtatious</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="main-popup">
+        <div class="popup">
+            <img class="popup-bond" src="/wp-content/themes/bondtheme/images/bond.svg" alt="bond">
+            <div class="popup-content">
+                <img src="/wp-content/themes/bondtheme/images/logo.svg" alt="bond next door" class="popup-logo">
+                <div class="popup-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi aperiam atque deleniti doloribus ducimus enim error et harum in, ipsum maiores numquam officiis quo quod totam velit voluptates voluptatum ipsum maiores numquam officiis quo quod totam velit voluptates voluptatum?
+                </div>
+                <div class="popup-numbers">
+                    <span><img src="/wp-content/themes/bondtheme/images/0.svg" alt="0"></span>
+                    <span><img src="/wp-content/themes/bondtheme/images/0.svg" alt="0"></span>
+                    <span><img src="/wp-content/themes/bondtheme/images/7.svg" alt="7"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 get_footer();
+
+
+
+
+
+
