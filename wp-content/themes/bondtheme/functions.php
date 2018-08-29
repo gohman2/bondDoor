96731f8ecfc3e863465dbacd29ac0988ebfec627
Vyacheslav <vasilenko.vyacheslav@catenamedia.com>
2018-08-29 12:30:35 +0300

    added jquery and custom.js

--- wp-content/themes/bondtheme/functions.php
+++ wp-content/themes/bondtheme/functions.php
@@ -123,6 +123,7 @@ function bondtheme_scripts() {
 	wp_enqueue_style( 'bondtheme-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'bondtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'bondtheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
---
+++ wp-content/themes/bondtheme/js/custom.js
@@ -0,0 +1,8 @@
+// Custom scripts
+(function($) {
+    $(document).ready(function () {
+        $('.wrapper').on('click', '.main-popup', function () {
+            $(this).fadeOut(400);
+        });
+    });
+})(jQuery);
