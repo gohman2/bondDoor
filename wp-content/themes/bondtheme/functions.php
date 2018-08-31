13ec1a7b12c531b030c2c6c5a19eb7b8df83ca76
Vyacheslav <vasilenko.vyacheslav@catenamedia.com>
2018-08-31 17:18:26 +0300

    city popup

--- wp-content/themes/bondtheme/functions.php
+++ wp-content/themes/bondtheme/functions.php
@@ -123,9 +123,11 @@ function bondtheme_scripts() {
 	wp_enqueue_style( 'bondtheme-style', get_stylesheet_uri() );
     wp_enqueue_style( 'google_fonts_opensans', "https://fonts.googleapis.com/css?family=Libre+Baskerville");
 	wp_enqueue_script( 'bondtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', array(), '20151215', true );
+    wp_enqueue_script( 'raphael', get_template_directory_uri() . '/js/raphael.js', array(), '20151215', true );
+//    wp_enqueue_script( 'diagram-init', get_template_directory_uri() . '/js/diagram-init.js', array(), '20151215', true );
+    wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'bondtheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'bondtheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
