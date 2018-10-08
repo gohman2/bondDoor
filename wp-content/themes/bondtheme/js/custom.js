a658a25ee0d791d5663f85dcb4f5f01bb2d5ceac
Khan <samir.khan.w@gmail.com>
2018-10-08 10:43:33 +0300

    Switched to OpenLayers map

--- wp-content/themes/bondtheme/js/custom.js
+++ wp-content/themes/bondtheme/js/custom.js
@@ -3,23 +3,24 @@
     $(document).ready(function () {
 
         function initInnerMap( lngVal, latVal ){
-            // Initialize the platform object:
-            var platform = new H.service.Platform({
-                app_id: 'KngCq2F5ZiDAoC5mHcOf',
-                app_code: 'B9eBCS_ZNlw3uV-F8JilqQ',
+
+            var map = new ol.Map({
+                controls : ol.control.defaults({
+                    attribution : false,
+                    zoom : false
+                }),
+                layers: [mapImage],
+                target: 'mapInner',
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([
+                        lngVal + 0.02,
+                        latVal
+                    ]),
+                    zoom: 6,
+                    maxResolution: 2800
+                })
             });
 
-            // Obtain the default map types from the platform object
-            var maptypes = platform.createDefaultLayers();
-
-            // Instantiate (and display) a map object:
-            var map = new H.Map(
-                document.getElementById('mapInner'),
-                maptypes.normal.map,
-                {
-                    zoom: 10,
-                    center: { lng: lngVal, lat: latVal }
-                });
         }
 //Diagram init
        function initDiagram( initName, initPercent, animationStatus ){
@@ -51,10 +52,6 @@
            });
        }
        function drowDiagram(hoverName, hoverPercent, initName, initPercent){
-console.log(hoverName);
-console.log(hoverPercent);
-console.log(initName);
-console.log(initPercent);
            if(typeof initPercent == "undefined"){
                if(initName == ""){
                    initPercent = hoverPercent;
@@ -170,14 +167,15 @@ console.log(initPercent);
                     $('.preloader-wrapper').show();
                 },
                 complete:function(msg){
-                    var title         = msg.responseJSON.title;
-                    var topContent    = msg.responseJSON.topContent;
-                    var fetureContent = msg.responseJSON.fetureContent;
-                    var description   = msg.responseJSON.description;
-                    var startFeture   = msg.responseJSON.startFeture;
-                    var startScore    = msg.responseJSON.startScore;
-                    var lngVal    = msg.responseJSON.lng;
-                    var latVal    = msg.responseJSON.lat;
+                    var response = msg.responseJSON;
+                    var title         = response.title;
+                    var topContent    = response.topContent;
+                    var fetureContent = response.fetureContent;
+                    var description   = response.description;
+                    var startFeture   = response.startFeture;
+                    var startScore    = response.startScore;
+                    var lngVal    = response.lng;
+                    var latVal    = response.lat;
                     $('.city-popup-box').fadeIn(200);
                     $("#mapInner").html('');
                     initInnerMap( lngVal, latVal );
@@ -204,16 +202,14 @@ function eventMapAjax( currentCity , $) {
     $('.main-nav, .socials').removeClass('shown');
 
     $('#diagram').find('svg').remove();
-    var eventCiryArray = currentCity.split("/");
 
     /* ajax query */
     var ajaxurl = myajax.url;
-    var cityId = eventCiryArray[1];
     var featureName = '';
 
     var ajaxdata = {
         action: "city-popup",
-        cityId: cityId,
+        cityId: currentCity,
         startFeture: featureName,
         nonce_code : myajax.nonce
     };
@@ -227,34 +223,35 @@ function eventMapAjax( currentCity , $) {
             $('.preloader-wrapper').show();
         },
         complete:function(msg){
-            var title         = msg.responseJSON.title;
-            var topContent    = msg.responseJSON.topContent;
-            var fetureContent = msg.responseJSON.fetureContent;
-            var description   = msg.responseJSON.description;
-            var startFeture   = msg.responseJSON.startFeture;
-            var startScore    = msg.responseJSON.startScore;
-            var lngVal    = msg.responseJSON.lng;
-            var latVal    = msg.responseJSON.lat;
+            var response = msg.responseJSON;
+            var title         = response.title;
+            var topContent    = response.topContent;
+            var fetureContent = response.fetureContent;
+            var description   = response.description;
+            var startFeture   = response.startFeture;
+            var startScore    = response.startScore;
+            var lngVal    = response.lng;
+            var latVal    = response.lat;
             $('.city-popup-box').fadeIn(200);
             $("#mapInner").html('');
 
-                // initInnerMap( lngVal, latVal );
-            var platform = new H.service.Platform({
-                app_id: 'KngCq2F5ZiDAoC5mHcOf',
-                app_code: 'B9eBCS_ZNlw3uV-F8JilqQ',
-            });
-
-            // Obtain the default map types from the platform object
-            var maptypes = platform.createDefaultLayers();
-
             // Instantiate (and display) a map object:
-            var map = new H.Map(
-                document.getElementById('mapInner'),
-                maptypes.normal.map,
-                {
-                    zoom: 10,
-                    center: { lng: lngVal, lat: latVal }
-                });
+            var map = new ol.Map({
+                controls : ol.control.defaults({
+                    attribution : false,
+                    zoom : false
+                }),
+                layers: [mapImage],
+                target: 'mapInner',
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([
+                        lngVal + 0.02,
+                        latVal
+                    ]),
+                    zoom: 6,
+                    maxResolution: 2800
+                })
+            });
 
             $(".popup-title").html(title);
             $(".city-popup-data").html(topContent);
