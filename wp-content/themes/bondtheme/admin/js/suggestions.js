edbb9d1f6ebf8c3b3a11d5a01b0c01022165488d
Khan <samir.khan.w@gmail.com>
2018-10-10 11:30:15 +0300

    fixed suggestions so that it is more accurate and if starts with

--- wp-content/themes/bondtheme/admin/js/suggestions.js
+++ wp-content/themes/bondtheme/admin/js/suggestions.js
@@ -1,12 +1,15 @@
 jQuery(function ($) {
     $("#titlewrap").append("<div id='suggestions'></div>");
     $("#title").on('keyup', function () {
+        var title_value = $("#title").val();
         $.getJSON(
-            'https://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=KngCq2F5ZiDAoC5mHcOf&app_code=B9eBCS_ZNlw3uV-F8JilqQ&country=GBR&query=' + $("#title").val(),
+            'https://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=KngCq2F5ZiDAoC5mHcOf&app_code=B9eBCS_ZNlw3uV-F8JilqQ&country=GBR&query=' + title_value,
             function (response) {
              var suggestions = response.suggestions, suggestions_cities = [], suggestions_html = '';
              for ( var n in suggestions ) {
-                 suggestions_cities.push(suggestions[n].address.city);
+                 if ( suggestions[n].address.city && suggestions[n].address.city.startsWith(title_value) ) {
+                     suggestions_cities.push(suggestions[n].address.city);
+                 }
              }
              var suggestions_unique = suggestions_cities.filter(function (item, i, arr) {
                  return i == arr.indexOf(item);
